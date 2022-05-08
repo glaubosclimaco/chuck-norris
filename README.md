d004e663477fcd4a3cbb26a59fa705abbbb7a4f2
glaubosclimaco <francisco.glaubos@ufma.br>
2022-05-07 23:47:24 -0300

    graphql: apollo client setup

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 
 
 ## About The Project
-This is a project to search for "facts" (jokes) about Chuck Norris. This repo was created with Next JS, using Typescript for frontend, Tailwindcss for styling and Cypress for tests.
+This is a project to search for "facts" (jokes) about Chuck Norris. This repository was created with Next JS, using Typescript for frontend, Tailwindcss for styling and Cypress for tests.
+
+### Working on a server module
 
 
 ## How to use
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,51 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import Button from '../components/button';
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import Button from '../components/button'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
+// 'uri' corresponds to the location of our GraphQL server
+// 'cache' to store and reuse query results
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+})
 
 export default function Home() {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-     
-
-       <br></br>
-       <p><Button href='random-joke' name='Random Joke' dataCy={'random-joke-btn'} onClick={undefined} /></p> 
-       <br></br>
-       <p> <Button href='joke-by-category' name='Joke by category' dataCy={'joke-by-category-btn'} onClick={undefined} /></p>
-       <br></br>
-       <p><Button href='joke-by-search' name='Search for a Joke' dataCy={'joke-by-search-btn'} onClick={undefined} /></p>
-    </Layout>
-  );
+    <ApolloProvider client={client}>
+      <Layout home>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+        <br></br>
+        <p>
+          <Button
+            href="random-joke"
+            name="Random Joke"
+            dataCy={'random-joke-btn'}
+            onClick={undefined}
+          />
+        </p>
+        <br></br>
+        <p>
+          {' '}
+          <Button
+            href="joke-by-category"
+            name="Joke by category"
+            dataCy={'joke-by-category-btn'}
+            onClick={undefined}
+          />
+        </p>
+        <br></br>
+        <p>
+          <Button
+            href="joke-by-search"
+            name="Search for a Joke"
+            dataCy={'joke-by-search-btn'}
+            onClick={undefined}
+          />
+        </p>
+      </Layout>
+    </ApolloProvider>
+  )
 }
