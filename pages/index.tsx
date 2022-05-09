c59b5e44129dff92fe67b3cf6b08e96c6d83c11a
glaubosclimaco <francisco.glaubos@ufma.br>
2022-05-09 06:46:24 -0300

    bug fix: buttons layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import Button from '../components/button'
+import utilStyles from '../styles/utils.module.css'
+
 
 export default function Home() {
   return (
